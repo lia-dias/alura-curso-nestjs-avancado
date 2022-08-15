dcb6c784b4fb471f40397723ab9efe693b4850e8
Lia Dias <liadias914@gmail.com>
2022-08-15 11:32:34 -0300

    (usuarios) add class validator to project and add to usuarios

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { UsuarioModule } from './modules';
 
 async function bootstrap() {
   const app = await NestFactory.create(UsuarioModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
--- src/modules/usuarios/usuario.model.ts
+++ src/modules/usuarios/usuario.model.ts
@@ -1,7 +1,32 @@
+import { IsString, IsNotEmpty, IsEmail } from 'class-validator';
+
 export default class Usuario {
     id: number;
+
+    @IsString({
+        message: 'nomeDeUsuario precisa ser uma string'
+    })
+    @IsNotEmpty({
+        message: 'nomeDeUsuario precisa ser informado'
+    })
     nomeDeUsuario: string;
+
+    @IsNotEmpty({
+        message: 'nomeDeUsuario precisa ser informado'
+    })
+    @IsEmail({}, {
+        message: 'email precisa ser válido'
+    })
+    email: string;
+
+    @IsNotEmpty({
+        message: 'senha é obrigatório'
+    })
     senha: string;
+    
+    @IsNotEmpty({
+        message: 'nomeCompleto é obrigatório.'
+    })
     nomeCompleto: string;
     dataDeCadastro: string;
 }
