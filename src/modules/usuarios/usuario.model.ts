0169bd9c18b773769a6bec1ffe2bf16c1ff4a6e8
Lia Dias <liadias914@gmail.com>
2022-08-15 14:52:40 -0300

    (usuarios) interceptors for model

--- src/modules/usuarios/usuario.model.ts
+++ src/modules/usuarios/usuario.model.ts
@@ -1,17 +1,21 @@
 import { IsString, IsNotEmpty, IsEmail } from 'class-validator';
+import { Exclude, Expose } from 'class-transformer';
 import { NomeDeUsuarioUnico } from './usuario.validation';
 
 export default class Usuario {
     id: number;
 
     @IsString({
-        message: 'nomeDeUsuario precisa ser uma string'
+        message: 'nome_de_usuario precisa ser uma string'
     })
     @IsNotEmpty({
-        message: 'nomeDeUsuario precisa ser informado'
+        message: 'nome_de_usuario precisa ser informado'
     })
     @NomeDeUsuarioUnico({
-        message: 'nomeDeUsuario deve ser único'
+        message: 'nome_de_usuario deve ser único'
+    })
+    @Expose({
+        name: 'nome_de_usuario'
     })
     nomeDeUsuario: string;
 
@@ -26,11 +30,21 @@ export default class Usuario {
     @IsNotEmpty({
         message: 'senha é obrigatório'
     })
+    @Exclude({
+        toPlainOnly: true
+    })
     senha: string;
     
     @IsNotEmpty({
         message: 'nomeCompleto é obrigatório.'
     })
+    @Expose({
+        name: 'nome_completo'
+    })
     nomeCompleto: string;
+
+    @Expose({
+        name: 'data_de_cadastro'
+    })
     dataDeCadastro: string;
 }
--- src/modules/usuarios/usuario.module.ts
+++ src/modules/usuarios/usuario.module.ts
@@ -1,4 +1,5 @@
-import { Module } from "@nestjs/common";
+import { ClassSerializerInterceptor, Module } from "@nestjs/common";
+import { APP_INTERCEPTOR } from "@nestjs/core";
 import UsuarioController from "./usuario.controller";
 import UsuarioService from "./usuario.service";
 import { UsuarioValidationUnicoConstraint } from "./usuario.validation";
@@ -6,6 +7,10 @@ import { UsuarioValidationUnicoConstraint } from "./usuario.validation";
 @Module({
     imports: [],
     controllers: [UsuarioController],
-    providers: [UsuarioService, UsuarioValidationUnicoConstraint]
+    providers: [UsuarioService, UsuarioValidationUnicoConstraint,
+    {
+        provide: APP_INTERCEPTOR,
+        useClass: ClassSerializerInterceptor
+    }]
 })
 export class UsuarioModule {}
